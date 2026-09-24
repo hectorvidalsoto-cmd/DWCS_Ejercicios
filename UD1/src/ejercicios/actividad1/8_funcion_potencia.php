38baf2e1ef0bd75602831d7c92c51bba3712db40
Hector Vidal Soto <hectorvidalsoto@gmail,com>
2026-09-24 16:40:01 +0200

    ejercicio 8 resuelto

---
+++ UD1/src/ejercicios/actividad1/8_funcion_potencia.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+<h1>Potencia</h1>
+
+    <form action="" method="POST">
+        <label for="base">Base</label>
+        <input type="text" name="base"><br>
+        <label for="exponente">Exponente</label>
+        <input type="text" name="exponente"><br>
+        <button type="submit">Enviar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        function potencia($base, $exponente) {
+            $resultado = 1;
+            for ($i = 0; $i < $exponente; $i++) {
+                $resultado = $resultado * $base;
+            }
+            return $resultado;
+        }
+
+        $base = $_POST['base'];
+        $exponente = $_POST['exponente'];
+        echo "La respuesta es " . potencia($base, $exponente);
+    }
+    ?>
+
+</body>
+</html>
